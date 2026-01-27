20c5bba8757c5d1a362d5e7ad8621dc8996b37c0
Mohit pal <pal001768@gmail.com>
2026-01-27 22:09:29 +0530

    change

---
+++ python/.py
@@ -0,0 +1,11 @@
+# #1 tax rate calculation
+# def calculation_tax(salary):
+#     if salary < 30000:
+#         tax=0.05
+#     elif 30000<=salary<=70000:
+#         tax=0.15
+#     else:
+#         tax=0.25
+#     return tax
+# salary=int(input("Enter salary: "))
+# print("Final tax rate: ",calculation_tax(salary)*100,"%")
