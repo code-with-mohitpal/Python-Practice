56b04694cb5e459f79c609a7ded2371a8d031b83
Mohit pal <pal001768@gmail.com>
2026-01-27 22:10:35 +0530

    change

---
+++ python4.py
@@ -0,0 +1,8 @@
+# #2 print even number between a and b
+# def print_even(a,b):
+#     for i in range(a,b+1):
+#         if i%2==0:
+#             print(i,end=" ")
+# a=int(input("Enter a: "))
+# b=int(input("Enter b: "))
+# print_even(a,b)
