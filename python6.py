61d4389a8fc735991ba61626ddceca25f0cd556c
Mohit pal <pal001768@gmail.com>
2026-01-27 22:12:59 +0530

    change

---
+++ python6.py
@@ -0,0 +1,8 @@
+# #print digits of a number
+# def print_digits(n):
+#     while n>0:
+#         digit=n%10
+#         print(digit)
+#         n//=10
+# n=int(input("enter number: "))
+# print_digits(n)
