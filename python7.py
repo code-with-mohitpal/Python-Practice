e6611314df2c2b1736000a1a08c20ed1dcecb16c
Mohit pal <pal001768@gmail.com>
2026-01-27 22:13:52 +0530

    change

---
+++ python7.py
@@ -0,0 +1,9 @@
+# #count number of digits
+# def count_digits(n):
+#     count=0
+#     while n>0:
+#         n//=10
+#         count +=1
+#     return count
+# n=int(input("enter number: "))
+# print("Number of digits: ",count_digits(n))
