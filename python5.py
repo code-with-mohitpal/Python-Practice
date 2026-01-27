df4c342869ec3ef5d2bda029dff26b0eaf652f6a
Mohit pal <pal001768@gmail.com>
2026-01-27 22:11:38 +0530

    change

---
+++ python5.py
@@ -0,0 +1,8 @@
+# #print odd number between a and b
+# def print_odd(a,b):
+#     for i in range(a,b+1):
+#         if i%2 !=0:
+#             print(i,end=" ")
+# a=int(input("Enter a: "))
+# b=int(input("Enter b: "))
+# print_odd(a,b)
