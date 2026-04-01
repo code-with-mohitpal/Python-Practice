e5850a3e3489f342e0ddd27eb4b44b034e7aa940
Mohit pal <pal001768@gmail.com>
2026-04-01 14:48:24 +0530

    Create Second largest number.py

---
+++ python/Second largest number.py
@@ -0,0 +1,11 @@
+def second_largest(arr):
+    first = second = float('-inf')
+    for num in arr:
+        if num > first:
+            second = first
+            first = num
+        elif num > second and num != first:
+            second = num
+    return second
+
+print(second_largest([10, 20, 4, 45, 99]))
