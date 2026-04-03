4a907ff61dd717b1d4fef42880d246da9392d93d
Mohit pal <pal001768@gmail.com>
2026-04-03 19:09:01 +0530

    Create Product.py

---
+++ python/Product.py
@@ -0,0 +1,15 @@
+def product_except_self(nums):
+    n = len(nums)
+    res = [1] * n
+    
+    left = 1
+    for i in range(n):
+        res[i] = left
+        left *= nums[i]
+    
+    right = 1
+    for i in range(n - 1, -1, -1):
+        res[i] *= right
+        right *= nums[i]
+    
+    return res
