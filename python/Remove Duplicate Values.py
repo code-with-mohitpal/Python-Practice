6824274250a80d457a83f94d1e39058d6f7570d0
Mohit pal <pal001768@gmail.com>
2026-04-07 17:22:36 +0530

    Create Remove Duplicate Values.py

---
+++ python/Remove Duplicate Values.py
@@ -0,0 +1,11 @@
+d = {'a': 1, 'b': 2, 'c': 1, 'd': 3}
+
+unique = {}
+seen = set()
+
+for key, value in d.items():
+    if value not in seen:
+        unique[key] = value
+        seen.add(value)
+
+print(unique)
