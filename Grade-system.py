849d2e5f95708b850d8e82113b58fa13968e7908
Mohit pal <pal001768@gmail.com>
2026-04-29 21:37:30 +0530

    Implement grade system function based on marks

---
+++ Grade-system.py
@@ -0,0 +1,14 @@
+def grade_system(marks):
+    if marks >= 90:
+        return "A+"
+    elif marks >= 75:
+        return "A"
+    elif marks >= 60:
+        return "B"
+    elif marks >= 50:
+        return "C"
+    else:
+        return "Fail"
+
+marks = int(input("Enter marks: "))
+print("Grade:", grade_system(marks))
