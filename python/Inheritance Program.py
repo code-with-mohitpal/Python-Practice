018259bed042067669094e848f1d1c12b5ecfff3
Mohit pal <pal001768@gmail.com>
2026-05-07 22:20:04 +0530

    Add inheritance example with Animal and Dog classes

---
+++ python/Inheritance Program.py
@@ -0,0 +1,16 @@
+class Animal:
+
+    def sound(self):
+        print("Animal makes sound")
+
+
+class Dog(Animal):
+
+    def bark(self):
+        print("Dog barks")
+
+
+d = Dog()
+
+d.sound()
+d.bark()
