e65b79204ce0dcea3d021c61061475c4935c89bb
Mohit pal <pal001768@gmail.com>
2026-05-07 22:20:44 +0530

    Add Bird and Cat classes with sound methods

---
+++ python/Polymorphism Program.py
@@ -0,0 +1,17 @@
+class Bird:
+
+    def sound(self):
+        print("Bird chirps")
+
+
+class Cat:
+
+    def sound(self):
+        print("Cat meows")
+
+
+b = Bird()
+c = Cat()
+
+b.sound()
+c.sound()
