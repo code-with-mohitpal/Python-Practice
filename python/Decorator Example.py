dab933c7298414310acc5c22989a57a4855ffef4
Mohit pal <pal001768@gmail.com>
2026-05-10 16:44:16 +0530

    Add smart_divide decorator for safe division
    Implement a decorator to handle division by zero.

---
+++ python/Decorator Example.py
@@ -0,0 +1,17 @@
+# Decorator in Python
+
+def smart_divide(func):
+    def inner(a, b):
+        if b == 0:
+            print("Cannot divide by zero")
+            return
+        return func(a, b)
+    return inner
+
+
+@smart_divide
+def divide(a, b):
+    print(a / b)
+
+divide(10, 2)
+divide(10, 0)
