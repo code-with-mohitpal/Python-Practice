4ccbd3bc4b9e3c340c4e96b03bcd8d4b4806cb70
Mohit pal <pal001768@gmail.com>
2026-05-20 21:39:33 +0530

    Add function to calculate sum of digits

---
+++ python/# Q5. Sum of digits.py
@@ -0,0 +1,13 @@
+# Q5. Sum of digits
+
+def sum_of_digits(n):
+    total = 0
+
+    while n > 0:
+        digit = n % 10
+        total += digit
+        n = n // 10
+
+    return total
+
+print(sum_of_digits(1234))
