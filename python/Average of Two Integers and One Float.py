9c033289541b3b3237aa9648f487ca4b4155fa31
Mohit pal <pal001768@gmail.com>
2026-06-06 22:03:23 +0530

    Add script to calculate average of two integers and a float

---
+++ python/Average of Two Integers and One Float.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+c = float(input("Enter a float number: "))
+
+average = (float(a) + float(b) + c) / 3
+
+print("Average =", average)
