8cca9580e46ad7f7d1150d8321764cb9fb607d58
Mohit pal <pal001768@gmail.com>
2026-06-06 22:08:34 +0530

    Create Integer Part and Fractional Part.py

---
+++ python/Integer Part and Fractional Part.py
@@ -0,0 +1,7 @@
+num = float(input("Enter a decimal number: "))
+
+integer_part = int(num)
+fractional_part = num - integer_part
+
+print("Integer Part =", integer_part)
+print("Fractional Part =", fractional_part)
