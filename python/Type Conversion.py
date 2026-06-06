f18aa5d00aec3d8752197bcb4f369e259d881828
Mohit pal <pal001768@gmail.com>
2026-06-06 22:04:01 +0530

    Create Type Conversion.py

---
+++ python/Type Conversion.py
@@ -0,0 +1,9 @@
+num_str = input("Enter a number: ")
+
+num_int = int(num_str)
+num_float = float(num_str)
+num_string = str(num_int)
+
+print(num_int, type(num_int))
+print(num_float, type(num_float))
+print(num_string, type(num_string))
